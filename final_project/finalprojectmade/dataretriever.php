76f0ba249ee2c88de24cee0848e26f5834407542
MRani <mrani@mail.walshcollege.edu>
2022-12-11 23:11:06 -0500

    Final_project

---
+++ final_project/finalprojectmade/dataretriever.php
@@ -0,0 +1,107 @@
+<?php
+
+//include database access class
+include("db_mysql.php");
+
+//declare the database instance
+$db = new DB_Sql;
+
+$function=$_POST['command'];
+
+//if we need to get the list of available projects, return the appropriate XML data
+if($function=="get_projects")
+	{
+		//array containing the list of projects
+		$project_list = array();
+
+		//the query to get the list of available projects
+		$query="SELECT * FROM Projects";
+
+		//do the query
+		$db->query($query);
+
+		while($db->next_record())
+		{
+			//store the database columns as variables
+			$project_num=$db->f("project_num");
+			$project_name=$db->f("project_name");
+			
+			//push the data onto the array that we intend to return
+			array_push($project_list, array("project_number" => $project_num, "project_full_name" => $project_name));
+				
+		}//end while, all available projects are pushed onto the project_list array	
+
+		//return the data in JSON format
+		echo json_encode(array("status" => "Success", "list_of_projects" => $project_list));
+		
+}//end if we need the list of current projects in the database
+
+//if we need to get the list of tasks by project, return the appropriate XML data
+if($function=="get_tasks")
+{		
+		//get the variables we need to run the query
+		$project_name=$_POST['project_name'];
+		error_log("*****");
+		error_log($project_name);
+
+		//array containing the list of projects
+		$task_list = array();
+		$query="SELECT * FROM tasks WHERE project_num= $project_num and (Status= 'Unassigned' or Status = 'In Process')";
+		//the query to get the list of tasks for the project
+		$query="SELECT * FROM Tasks where project_num=(select project_num from projects where project_name='$project_name')";
+	
+		error_log($query);
+
+		//do the query
+		$db->query($query);
+
+		while($db->next_record())
+		{
+			//store the database columns as variables
+			$task_num=$db->f("task_num");
+			$description=$db->f("Description");
+			$task_status=$db->f("Status");
+			error_log($description);
+			//push the data onto the array that we intend to return
+			array_push($task_list, array("task_num" => $task_num, "task_description" => $description, "task_status" => $task_status));
+			$query= "SELECT * FROM Tasks where status == Unassigned";
+    				
+    	}//end while, all available tasks for the specified project are pushed onto the task_list array	
+
+		//return the data in JSON format
+		echo json_encode(array("status" => "Success", "list_of_tasks" => $task_list));
+       
+}//end if we need the list of tasks
+if($function=="validate_user")
+{		
+		//get the variables we need to run the query
+		$username=$_POST['username'];
+		$passwd=$_POST['passwd'];
+		
+		//array containing the list of projects
+		$task_list = array();
+
+		//the query to get the list of tasks for the project
+		$query="SELECT COUNT(*) USERCNT FROM Creds where username='$username' and password='$passwd'";
+		error_log($query);
+
+		//do the query
+		$db->query($query);
+
+		while($db->next_record())
+		{
+			//store the database columns as variables
+			$count_rec=$db->f("USERCNT");
+			error_log($count_rec);
+			//push the data onto the array that we intend to return
+			
+			//array_push($user_list, array("usr_cnt" => $count_rec));
+    				
+    	}//end while, all available tasks for the specified project are pushed onto the task_list array	
+
+		//return the data in JSON format
+		echo json_encode($count_rec,JSON_NUMERIC_CHECK);
+		//echo json_encode(array("status" => "Success", "list_of_tasks" => $task_list));
+
+}
+?>
---
+++ final_project/finalprojectmade/js/application.js
@@ -0,0 +1,230 @@
+$(document).ready(function(){
+    $("#select_project_state").hide();
+    $("#display_task_info_state").hide();
+    $("#add_modify_task_state").hide();
+    $("#display_project_info_state").hide();
+    $.ajaxSetup({ cache: true });
+    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+        FB.init({
+          appId: '551739683458605',
+          version: 'v2.7'
+        });
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
+    });
+    $("#login_button").click(function(){
+        validateUser();
+        return false;    
+    });
+    $("#login_fb").click(function(){
+       FB.login(function(response){
+           statusChangeCallback(response);
+       });
+    })
+    $("#modify_button").click(function(){
+       
+       modifyTasks();
+        return false;  
+
+    });
+
+    $("#add_button").click(function(){
+        addTasks();
+        return false;   
+    });
+    
+    $("#project_button").click(function(){
+        getTasks();
+        return false;   
+    });
+    
+    $("p").mouseover(function(){
+        $("p").css("background-color", "yellow");
+      });
+      $("p").mouseout(function(){
+        $("p").css("background-color", "lightgray");
+      });
+      
+});
+//transition functions 
+function myFunction() {
+    document.getElementById("select_project_state").style.transition = "all 2s";}
+function myFunction() {
+   document.getElementById("display_task_info_state").style.transition = "all 4s";}
+function over(){
+   document.getElementById("midify_button").innerHTML="Edit Project Tasks"; }
+
+//End transitions 
+//End document ready functionality
+//Begin validUser function
+function validateUser()
+{
+   var form_username = $("#login_username").val();
+   if (form_username == "a")
+   {
+    $( "#login_form").hide();
+    $("#login_fb").hide();
+    $("#select_project_state").show();
+    $("#display_task_info_state").show();
+    $("#add_modify_task_state").show();
+    $("#display_project_info_state").hide();
+    getProjects();
+   }
+   else
+   {
+    $( "#login_form" ).effect( "shake" );
+   }
+
+}
+
+//Begin getProject function
+function getProjects()
+{
+   var retriever_command = "get_projects";
+   $.post('dataretriever.php', {command: retriever_command}, function(data){
+    
+       var count = 0;
+       $("#project_selector").empty();
+       while(count < data.list_of_projects.length)
+       {
+           $("#project_selector").append("<option value='" + data.list_of_projects[count].project_number+"'>" + data.list_of_projects[count].project_full_name +"</option>");
+           
+           count++;
+       }// end while loop
+       $("#project_selector").prepend("<option value='projs'>----SELECT PROJECT----</option>");
+       $("#project_selector")[0].selectedIndex = 0
+
+       
+   }, "json").fail(function(){alert("Web Service Call Failed");});
+}
+//End getProject
+//Begin getTasks function
+function  getTasks()
+{
+    var elem = document.getElementById("project_selector");
+    var val2 = elem.options[elem.selectedIndex].value;
+    var text2 = elem.options[elem.selectedIndex].text;
+    //alert("GETTING TASKS");
+    //alert(text2);
+   
+    var retriever_command = "get_tasks";
+
+   $.post('dataretriever.php', {command: retriever_command,project_name: text2 }, function(data){
+    
+       var count = 0;
+       
+       //$("#tsk_num").empty();
+       //$("#tsk_desc").empty();
+       var t = "";
+       /*while(count < data.list_of_tasks.length)
+       {
+           $("#task_selector").append("<option value='" + data.list_of_tasks[count].task_status+ "</option>");
+           $("#tsk_num").text(data.list_of_tasks[count].task_num);
+           $("#tsk_desc").text(data.list_of_tasks[count].task_description);
+            error_log(data.list_of_tasks[count].task_description);
+
+            
+            for (var i = 0; i < posts_array.length; i++){
+
+                var tr = "<tr>";
+                    tr += "<td>"+posts_array[i][0]+"</td>";
+                    tr += "<td>"+posts_array[i][1]+"</td>";
+                    tr += "<td>"+posts_array[i][2]+"</td>";
+                    tr += "<td>"+posts_array[i][3]+"</td>";
+                    tr += "</tr>";
+                    t += tr;
+            }
+           count++;
+       }// end while loop*/
+
+       t = "<tr><th>Task Number</th><th>Task Description</th><th>Task Status</th></tr>";
+       for (var i = 0; i < data.list_of_tasks.length; i++){
+
+        var tr = "<tr>";
+            tr += "<td>"+data.list_of_tasks[i].task_num+"</td>";
+            tr += "<td>"+data.list_of_tasks[i].task_description+"</td>";
+            tr += "<td>"+data.list_of_tasks[i].task_status+"</td>";
+            tr += "</tr>";
+            t += tr;
+    }
+    //alert(t);
+       //document.getElementById("display_task_info_table") += t;
+       //$( "#display_task_info_table" ).empty();
+       //$( "#display_task_info_table" ).append(index.html);
+       document.getElementById("display_task_info_table");
+       $("#display_task_info_table tr").remove();
+       $("#display_task_info_table").append(t);
+
+       $("#display_task_info_table").show();
+   }, "json").fail(function(){alert("Web Service Call Failed");});
+}
+
+//end get tasks function
+
+function statusChangeCallback(response)
+{
+    if (response.status=== "connected")
+    {
+        $("#login_state").hide();
+        $("#select_project_state").show();
+    }
+    else
+    {
+       console.log("Please log in");
+    }
+    console.log(response)
+}
+
+function project_select_status_change()
+{
+    var elem = document.getElementById("project_selector");
+    var val2 = elem.options[elem.selectedIndex].value;
+    var text2 = elem.options[elem.selectedIndex].text;
+    alert(text2);
+   
+    var xhttp = new XMLHttpRequest();
+     xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("project_selector").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "dataretriever.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("t=" + Math.random()+"&project_name="+text2+"&command=get_tasks");
+  $("#result").html(result);
+  $("#display_task_info_state").show();
+  
+  
+    
+}
+function modifyTasks()
+
+
+{ 
+    
+    $( "#login_form").hide();
+    $("#login_fb").hide();
+    $("#select_project_state").hide();
+    $("#display_task_info_state").hide();
+    $("#add_modify_task_state").show();
+    $("#display_project_info_state").show();
+    $("#addInfo").hide();
+    $("#modifyInfo").show();
+
+}
+
+function addTasks()
+{
+    $( "#login_form").hide();
+    $("#login_fb").hide();
+    $("#select_project_state").hide();
+    $("#display_task_info_state").hide();
+    $("#add_modify_task_state").show();
+    $("#display_project_info_state").show();
+    $("#addInfo").show();
+    $("#modifyInfo").hide();
+}
+
+
+//function getProjects()
